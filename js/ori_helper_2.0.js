0f9cf97d3996268b8241394c44170053cab91acd
Denis <denis.obukhov@oggettoweb.com>
2015-06-16 16:06:53 +0300

    Update ori script options

--- js/ori_helper_2.0.js
+++ js/ori_helper_2.0.js
@@ -1,16 +1,27 @@
-function showOriFancybox(status, content) {
-    jQuery.fancybox({
+function showOriFancybox(status, options) {
+    var defaults = {
         'type': 'inline',
-        'content': content,
+        'content': '#update',
         'autoScale': false,
         'autoDimensions': false,
-        'width': '800',
-        'height': '700',
-        'onClosed': function () {
-            onClosedOriFancybox(status);
-        },
-        'onComplete': function() {
-            onCompleteOriFancybox(status);
-        }
+        'width': 800,
+        'height':700,
+        'onComplete': function(){},
+        'onClosed': function(){}
+    };
+    var opts = $.extend( {}, defaults, options );
+    jQuery.fancybox({
+        'type': opts.type,
+        'content': opts.content,
+        'autoScale': opts.autoScale,
+        'autoDimensions': opts.autoDimension,
+        'width': opts.width,
+        'height': opts.height,
+        'onClosed': opts.onClosed,
+        'onComplete': opts.onComplete
     });
+}
+
+function closeOriFancybox() {
+    jQuery.fancybox.close();
 }
